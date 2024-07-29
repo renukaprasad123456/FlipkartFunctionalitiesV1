bc749202c03e1b00991ec41c57c442c54d2a32f7
renukaprasad123456 <renukaprasadgs087@gmail.com>
2024-07-29 20:39:02 +0530

    Renukaprasad: This is my first commit as on 29-07-24

---
+++ Flipkart.com/src/test/java/Com/Flipkart/TestCases/SearchFunctionality.java
@@ -0,0 +1,22 @@
+package Com.Flipkart.TestCases;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SearchFunctionality {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://www.flipkart.com/");
+
+	}
+
+}
